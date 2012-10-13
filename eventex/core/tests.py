a8a9f3be93244baf73e85a9b985b4c8585ab165b
RafaTrevisan <rafa.trevisan@gmail.com>
2012-10-13 13:51:04 -0300

    All your base are belong to us

---
+++ eventex/core/tests.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from django.test import TestCase
+
+class HomepageTest(TestCase):
+    def test_get(self):
+        'GET / must return status code 200'
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'index.html')
